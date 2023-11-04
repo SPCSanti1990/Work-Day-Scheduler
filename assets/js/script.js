410dfb80c2396928ae3ae8ef33f8ded2ad417d0a
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-11-04 18:26:27 -0400

    fixed row class ids, fixed saved text recall, added block color change based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
   // Handle saving text in the textarea to local storage
 
   $(".saveBtn").on("click", function () {
@@ -18,38 +12,50 @@ $(function () {
     localStorage.setItem(time, text);
   })
    // Load saved text from local storage
-   $("#hour9 .description").val(localStorage.getItem("nineAM"));
-   $("#hour10 .description").val(localStorage.getItem("tenAM"));
-   $("#hour11 .description").val(localStorage.getItem("elevenAM"));
-   $("#hour12 .description").val(localStorage.getItem("twelvePM"));
-   $("#hour13 .description").val(localStorage.getItem("onePM"));
-   $("#hour14 .description").val(localStorage.getItem("twoPM"));
-   $("#hour15 .description").val(localStorage.getItem("threePM"));
-   $("#hour16 .description").val(localStorage.getItem("fourPM"));
-   $("#hour17 .description").val(localStorage.getItem("fivePM"));
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+   $("#hour9 .description").val(localStorage.getItem("hour9"));
+   $("#hour10 .description").val(localStorage.getItem("hour10"));
+   $("#hour11 .description").val(localStorage.getItem("hour11"));
+   $("#hour12 .description").val(localStorage.getItem("hour12"));
+   $("#hour13 .description").val(localStorage.getItem("hour13"));
+   $("#hour14 .description").val(localStorage.getItem("hour14"));
+   $("#hour15 .description").val(localStorage.getItem("hour15"));
+   $("#hour16 .description").val(localStorage.getItem("hour16"));
+   $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-  // If current time is > time block change row color to past color in css file
-  // If current time is = time block change row color to present color in css file
-  // If current time is < time block change row color to future color in css file
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  // Handle saving text in the textarea to local storage
- 
+  function timeTracker() {
+    //let currentTime = 12; Test variable to change time slot change color without waiting for a hour to pass 
+    let currentTime = dayjs().format('MMM, D YYYY, h:mm:ss a');
 
-  // Load saved text from local storage
+    $(".time-block").each(function () {
+      let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+// If current time is > time block change row color to past color in css file
+      if (currentTime > timeBlock) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+ // If current time is = time block change row color to present color in css file
+      else if ( currentTime === timeBlock) {
+        $(this).addClass("present")
+        $(this).removeClass("past")
+        $(this).removeClass("future")
+      }
+// If current time is < time block change row color to future color in css file
+      else {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present");
+      }
+    })
+    // Call clock funtion to keep time updated
+    clock();
+  }
 
-  //
   // Function to keep current time and date updated on screen
     function clock() {
-      let todayTime = dayjs().format('MMM, D YYYY, h:mm:ss');
+      let todayTime = dayjs().format('MMM, D YYYY, h:mm:ss a');
       $('#currentDay').text(todayTime);
     }
-    setInterval(function(){clock();},1000);
+    // Set an interval of 1 second to recall timeTracker function every second
+    setInterval(function(){timeTracker();},1000);
 });
